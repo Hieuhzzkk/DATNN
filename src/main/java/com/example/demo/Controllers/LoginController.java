d1b670e8090a7bb142097b1a252305dbf108bf09
MinhDung <minhdung1832@gmail.com>
2022-11-12 16:21:19 +0700

    Code Login

---
+++ src/main/java/com/example/demo/Controllers/LoginController.java
@@ -0,0 +1,26 @@
+package com.example.demo.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.repository.ProductRepository;
+
+
+/**
+ * @author DongTHD
+ *
+ */
+@Controller
+public class LoginController {
+
+	@Autowired
+	ProductRepository productRepository;
+
+	@GetMapping(value = "/login")
+	public String login() {
+
+		return "web/login";
+	}
+
+}
---
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entities.Comment;
+
+
+/**
+ * @author 
+ *
+ */
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long>{
+
+}

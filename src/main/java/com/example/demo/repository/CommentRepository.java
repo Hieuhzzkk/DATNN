03e460cb9e355dcfda289023077b260a46897fed
MinhDung <minhdung1832@gmail.com>
2022-11-13 20:01:48 +0700

    Register

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entities.Comment;
-import vn.fs.entities.Comment;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>{
 
